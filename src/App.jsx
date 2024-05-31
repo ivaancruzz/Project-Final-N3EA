807f1d3c2539a4f00fba3cbceec058a0d6077060
ELIAS <elias.alasia.moreno@gmail.com>
2024-05-31 16:31:00 -0500

    Finalizado instalacion de los elementos para le inicio de proyecto final, se probo con un button que funciona todo acorde.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="text-center h-full">
+    <button className= " bg-orange-300 font-bold border rounded-md">
+        Hola Project Final
+      </button>
+    </div>
+   
+    </>
+  )
+}
+
+export default App
