42622ce83dbc4343c2130bbc9613a2bb2cfd7080
ELIAS <elias.alasia.moreno@gmail.com>
2024-06-04 13:06:20 -0500

    Realice componetizacion del modal intento de conexion a la Api, y corrigiendo errores falta llamar mas de la api

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -7,7 +7,7 @@ import { TemperaturTime } from "./TemperaturTime"
 export const Time = () => {
     return (
         <>
-         <div className="w-full md:w-3/4 p-4 flex flex-col h-screen">
+         <div style={{ width: "84%" ,padding: "3rem", justifyContent: "space-around", gap: "6rem" }} className=" md:w-3/4 flex flex-col h-screen ">
             <WeeklyTime/>
             <TemperaturTime/>
          </div>
--- src/hooks/WeatherContext.jsx
+++ src/hooks/WeatherContext.jsx
@@ -4,6 +4,8 @@ const WeatherContext = createContext();
 
 const WeatherProvider = ({ children }) => {
     const [weatherData, setWeatherData] = useState(null);
+    const [location, setLocation] = useState({ lat: 44.34, lon: 10.99 });
+    const [city, setCity] = useState('');
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -24,17 +26,73 @@ const WeatherProvider = ({ children }) => {
         }
     };
 
+    const fetchCityName = async (lat, lon) => {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c739b37edac93e5b65459fa362b12a26`);
+            const data = await response.json();
+            setCity(data.name);
+        } catch (error) {
+            console.error('Error fetching city name:', error);
+        }
+    };
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setLocation({
+                        lat: position.coords.latitude,
+                        lon: position.coords.longitude,
+                    });
+                },
+                (error) => {
+                    console.error('Error getting location:', error);
+                }
+            );
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+        }
+    };
+
+    const searchLocation = (ubicacion) => {
+        fetch(``)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.coord) {
+                    setLocation({ lat: data.coord.lat, lon: data.coord.lon });
+                } else {
+                    console.error('Location not found');
+                }
+            })
+            .catch((error) => {
+                console.error('Error searching location:', error);
+            });
+    };
+
+    const kelvinToCelsius = (kelvin) => (kelvin - 273.15).toFixed(2);
+    const kelvinToFahrenheit = (kelvin) => ((kelvin - 273.15) * 9 / 5 + 32).toFixed(2);
+
     useEffect(() => {
-        fetchWeather(44.34, 10.99); 
-    }, []);
+        fetchWeather(location.lat, location.lon);
+        fetchCityName(location.lat, location.lon);
+    }, [location]);
 
     return (
-        <WeatherContext.Provider value={{ weatherData, loading, error, fetchWeather }}>
+        <WeatherContext.Provider
+            value={{
+                weatherData,
+                city,
+                getLocation,
+                kelvinToCelsius,
+                kelvinToFahrenheit,
+                searchLocation,
+                loading,
+                error,
+            }}
+        >
             {children}
         </WeatherContext.Provider>
     );
 };
 
 export { WeatherProvider, WeatherContext };
-
-/* Define el contexto y el proveedor del clima. */
